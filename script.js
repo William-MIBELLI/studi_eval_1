11ff7e1a65cc6a4a1a820c7187dd0d286235dced
William MIBELLI <william.mibelli@gmail.com>
2022-07-24 19:59:40 +0200

    ajout des pages galeries et calendrier

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let contact_btn = document.getElementById('contact-btn')
 let articles = document.querySelectorAll('.article')
 let contact = document.getElementById('contact')
+let pic = document.querySelectorAll('.pic')
 console.log(articles)
 
 contact_btn.addEventListener('click' , () => {
@@ -13,4 +14,10 @@ contact_btn.addEventListener('click' , () => {
         item.style.display = 'none'
     })
     contact.style.display = 'flex'
+})
+
+pic.forEach(item => {
+    item.addEventListener('click', (e) => {
+        alert(e.target.className)
+    })
 })
