15e726ae6698ce0ee29f0b0a9ebd30608a72a0c9
William MIBELLI <william.mibelli@gmail.com>
2022-07-25 00:11:27 +0200

    refonte page d'accueil et calendrier

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-let contact_btn = document.getElementById('contact-btn')
-let articles = document.querySelectorAll('.article')
-let contact = document.getElementById('contact')
-let pic = document.querySelectorAll('.pic')
-console.log(articles)
+let chantier = document.querySelectorAll('.chantier')
 
-contact_btn.addEventListener('click' , () => {
-    articles.forEach((item,key) => {
-        // if(key%2==0){
-        //     item.className = 'animateLeft'
-        // }else{
-        //     item.className = 'animateRight'
-        // }
-        item.style.display = 'none'
-    })
-    contact.style.display = 'flex'
-})
-
-pic.forEach(item => {
-    item.addEventListener('click', (e) => {
-        alert(e.target.className)
-    })
+chantier.forEach(item => {
+    item.addEventListener('click', () => alert('Page en chantier, revenez bientôt ! :)'))
 })
