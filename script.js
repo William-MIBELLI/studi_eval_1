7c79087fcf91958e7a512ce5d4509810183d16d8
William MIBELLI <william.mibelli@gmail.com>
2022-07-25 16:32:20 +0200

    responsive portable et tablette

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let chantier = document.querySelectorAll('.chantier')
+let burger = document.querySelector('.burger')
+let menu = document.getElementsByTagName('nav')
+
 
 chantier.forEach(item => {
     item.addEventListener('click', () => alert('Page en chantier, revenez bientôt ! :)'))
+})
+
+
+burger.addEventListener('click', () => {
+    menu[0].style.display == 'none'? 'flex' : 'none'
+    alert(menu)
 })
